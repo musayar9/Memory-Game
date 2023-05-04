2d89fbb4bebccb029f0aeb54c65ed1b18a861140
Musa SAYAR <musasayar67@gmail.com>
2023-05-04 15:20:38 +0300

    header layout

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,19 +8,20 @@ function Header() {
     const dispatch = useDispatch()
     return (
 
-        <div className="container d-flex align-items-center justify-content-between mt-2 p-3 rounded-2 " style={{ backgroundColor: "#202020", zIndex: "100" }}>
+        <div className="container d-block  d-md-flex align-items-center justify-content-between mt-2 p-3 rounded-2 " style={{ backgroundColor: "#202020", zIndex: "100" }}>
 
-            <div>
-                <p className="text-center fs-4 text-light fw-bolder" >
+            <div className='order-2 order-md-1'>
+            <p className='text-capitalize text-danger fw-bolder fs-3 order-1 order-lg-2 d-flex d-md-none '>  To win you must exceed 400 points</p>
+                <p className="text-center fs-4 text-light fw-bolder order-2 order-lg-1" >
                     Your's Point: <span className='text-primary'>{point}</span>
                 </p>
             </div>
-            <div className='mt-4'>
-                <p className='text-capitalize text-danger fw-bolder fs-3 '>  To win you must exceed 400 points</p>
+            <div className='mt-4 order-1 order-md-2'>
+                <p className='text-capitalize text-danger fw-bolder fs-3 order-1 order-lg-2 d-none d-md-flex '>  To win you must exceed 400 points</p>
 
                 <p className={`${count < 10 ? "text-danger" : "text-white"} badge fs-3 fw-3  bg-primary px-4 py-2`}> {count}</p>
             </div>
-            <div>
+            <div className='order-3 order-md-3 '>
                 <button className='btn btn-primary' onClick={() => dispatch(resetFrameworks())}><VscDebugRestart size={18} /> Reload Game</button>
             </div>
         </div>
